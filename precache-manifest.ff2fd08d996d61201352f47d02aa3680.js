1fa4a56e70f850da592610e739cbd23883f776d0
Paolo D'Elia <paolo.delia99@gmail.com>
2020-03-10 16:35:42 +0100

    Updates

---
+++ precache-manifest.ff2fd08d996d61201352f47d02aa3680.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31a825ccce79bbcd491d60051b176946",
+    "url": "./index.html"
+  },
+  {
+    "revision": "08c91bd8de592d2ee6c8",
+    "url": "./static/css/2.27f6e87d.chunk.css"
+  },
+  {
+    "revision": "764e15a17ebdba206c05",
+    "url": "./static/css/main.93e749e6.chunk.css"
+  },
+  {
+    "revision": "08c91bd8de592d2ee6c8",
+    "url": "./static/js/2.dd6a1483.chunk.js"
+  },
+  {
+    "revision": "9db3fe70c8965c574c9717aa6d286ca5",
+    "url": "./static/js/2.dd6a1483.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "764e15a17ebdba206c05",
+    "url": "./static/js/main.ae15822f.chunk.js"
+  },
+  {
+    "revision": "7e0fe827dffebd1f0431",
+    "url": "./static/js/runtime-main.edd5d170.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "49f9528195a7408c9e8c3e97715d0356",
+    "url": "./static/media/movie.49f95281.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/netflix-clone/precache-manifest.ebe4581073466c9ee4ca7e81d19abbb6.js"
+  "./precache-manifest.ff2fd08d996d61201352f47d02aa3680.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/netflix-clone/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
